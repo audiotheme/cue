bca0a630f0110d09435b4b663be228a57fedd597
Brady Vercher <brady@blazersix.com>
2014-08-18 12:56:30 -0700

    Use the background image as a fallback for each track's artwork.

--- assets/js/cue-mejs.js
+++ assets/js/cue-mejs.js
@@ -17,6 +17,11 @@ window.cue = window.cue || {};
 				src: player.options.cueBackgroundUrl
 			})).find( '.mejs-player-background' );
 
+			// Set each track to use the background image as artwork if it doesn't have artwork.
+			$.each( player.options.cuePlaylistTracks, function( index, track ) {
+				player.options.cuePlaylistTracks[ index ].thumb.src = track.thumb.src || player.options.cueBackgroundUrl;
+			});
+
 			player.$node.on( 'setTrack.cue', function( e, track, player ) {
 				track.thumb = track.thumb || {};
 
