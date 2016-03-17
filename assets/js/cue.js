b9183a15c335222976ac2803afe14a6b40506649
Brady Vercher <brady@blazersix.com>
2016-03-17 12:06:16 -0700

    Make the initialize method public and call it on pjax:end.

--- assets/js/cue.js
+++ assets/js/cue.js
@@ -51,74 +51,79 @@ window.cue = window.cue || {};
 
 	$html.toggleClass( 'no-css-filters', ! cue.settings.hasCssFilters ).toggleClass( 'no-svg-filters', ! cue.settings.hasSvgFilters );
 
-	// Document ready.
-	$( document ).ready(function( $ ) {
-		// Initialize the playlists.
-		$( '.cue-playlist' ).each(function() {
-			var $playlist = $( this ),
-				$data = $playlist.closest( '.cue-playlist-container' ).find( '.cue-playlist-data' ),
-				backgroundUrl = '',
-				data;
+	$.extend( cue, {
+		initialize: function() {
+			// Initialize the playlists.
+			$( '.cue-playlist' ).each(function() {
+				var $playlist = $( this ),
+					$data = $playlist.closest( '.cue-playlist-container' ).find( '.cue-playlist-data' ),
+					backgroundUrl = '',
+					data;
 
-			if ( $data.length ) {
-				data = $.parseJSON( $data.first().html() );
+				if ( $data.length ) {
+					data = $.parseJSON( $data.first().html() );
 
-				// Set the background image source.
-				if ( 'thumbnail' in data ) {
-					backgroundUrl = data.thumbnail;
-				}
-			}
-
-			if ( ! cue.settings.hasCssFilters && cue.settings.hasSvgFilters ) {
-				if ( ! $( '#cue-filter-blur' ).length ) {
-					$( 'body' ).append( '<svg id="cue-filter-svg" style="position: absolute; bottom: 0"><filter id="cue-filter-blur"><feGaussianBlur class="blur" stdDeviation="20" color-interpolation-filters="sRGB"/></filter></svg>' );
+					// Set the background image source.
+					if ( 'thumbnail' in data ) {
+						backgroundUrl = data.thumbnail;
+					}
 				}
 
-				$playlist.on( 'backgroundCreate.cue', function( e, player ) {
-					player.container.find( '.mejs-player-background' ).css( 'filter', 'url(\'' + window.location.href + '#cue-filter-blur\')' );
+				if ( ! cue.settings.hasCssFilters && cue.settings.hasSvgFilters ) {
+					if ( ! $( '#cue-filter-blur' ).length ) {
+						$( 'body' ).append( '<svg id="cue-filter-svg" style="position: absolute; bottom: 0"><filter id="cue-filter-blur"><feGaussianBlur class="blur" stdDeviation="20" color-interpolation-filters="sRGB"/></filter></svg>' );
+					}
+
+					$playlist.on( 'backgroundCreate.cue', function( e, player ) {
+						player.container.find( '.mejs-player-background' ).css( 'filter', 'url(\'' + window.location.href + '#cue-filter-blur\')' );
+					});
+				}
+
+				$playlist.cuePlaylist({
+					cueBackgroundUrl: backgroundUrl,
+					cueResponsiveProgress: true,
+					cueSelectors: {
+						playlist: '.cue-playlist'
+					},
+					cueSkin: 'cue-skin-default',
+					defaultAudioHeight: 0,
+					features: [
+						'cuebackground',
+						'cueartwork',
+						'cuecurrentdetails',
+						'cueprevioustrack',
+						'playpause',
+						'cuenexttrack',
+						'volume',
+						'progress',
+						'current',
+						'duration',
+						'cueplaylist',
+						'audiothememark'
+					]
+				}).cueMediaClasses({
+					breakpoints: [{
+						type: 'max-width',
+						size: 480
+					}, {
+						type: 'max-width',
+						size: 380
+					},
+					{
+						type: 'max-width',
+						size: 300
+					},
+					{
+						type: 'max-width',
+						size: 200
+					}]
 				});
-			}
-
-			$playlist.cuePlaylist({
-				cueBackgroundUrl: backgroundUrl,
-				cueResponsiveProgress: true,
-				cueSelectors: {
-					playlist: '.cue-playlist'
-				},
-				cueSkin: 'cue-skin-default',
-				defaultAudioHeight: 0,
-				features: [
-					'cuebackground',
-					'cueartwork',
-					'cuecurrentdetails',
-					'cueprevioustrack',
-					'playpause',
-					'cuenexttrack',
-					'volume',
-					'progress',
-					'current',
-					'duration',
-					'cueplaylist',
-					'audiothememark'
-				]
-			}).cueMediaClasses({
-				breakpoints: [{
-					type: 'max-width',
-					size: 480
-				}, {
-					type: 'max-width',
-					size: 380
-				},
-				{
-					type: 'max-width',
-					size: 300
-				},
-				{
-					type: 'max-width',
-					size: 200
-				}]
 			});
-		});
+		}
 	});
+
+	$( document )
+		.ready( cue.initialize )
+		.on( 'pjax:end', cue.initialize );
 
 })( this, jQuery );
