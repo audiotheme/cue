6080f8d9328c21baab3fdb2ed5e4feea94a46a48
Brady Vercher <brady@blazersix.com>
2016-05-16 12:14:44 -0700

    Add a Shipit config file for deployment.

---
+++ shipitfile.js
@@ -0,0 +1,45 @@
+/*global Promise:false */
+
+var fs = require('fs'),
+	project = require( __dirname + '/package.json' );
+
+module.exports = function ( shipit ) {
+	shipit.initConfig({
+		demo: {
+			servers: 'deploy@demo.audiotheme.com',
+			deployRoot: '/usr/share/nginx/html/demo.audiotheme.com/public/wp-content/plugins/'
+		},
+		staging: {
+			servers: 'deploy@staging.cedaro.com',
+			deployRoot: '/srv/www/staging.cedaro.com/public/wp-content/plugins/'
+		}
+	});
+
+	shipit.task( 'deploy', function() {
+		shipit.archiveFile = project.name + '-' + project.version + '.zip';
+		shipit.deployPath = shipit.config.deployRoot;
+
+		return createDeploymentPath()
+			.then( copyProject() )
+			.then( unpackDeployment );
+	});
+
+	function createDeploymentPath() {
+		return shipit.remote( 'mkdir -p ' + shipit.deployPath );
+	}
+
+	function copyProject() {
+		return shipit.remoteCopy( 'dist/' + shipit.archiveFile, shipit.deployPath );
+	}
+
+	function unpackDeployment() {
+		var cmd = [];
+
+		cmd.push( 'cd ' + shipit.deployPath );
+		cmd.push( 'rm -rf ' + project.name );
+		cmd.push( 'unzip -q ' + shipit.archiveFile );
+		cmd.push( 'rm ' + shipit.archiveFile );
+
+		return shipit.remote( cmd.join( ' && ' ) );
+	}
+};
