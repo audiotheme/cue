b972dfdc8f589f158664486fc8a1001575a1c099
Brady Vercher <brady@blazersix.com>
2016-07-14 10:05:11 -0700

    Introduce 'cue_playlist_top' and 'cue_playlist_bottom' actions.

--- templates/playlist.php
+++ templates/playlist.php
@@ -1,4 +1,6 @@
 <div class="<?php echo esc_attr( $classes ); ?>" itemscope itemtype="http://schema.org/MusicPlaylist">
+	<?php do_action( 'cue_playlist_top', $post, $tracks, $args ); ?>
+
 	<meta itemprop="numTracks" content="<?php echo count( $tracks ); ?>" />
 
 	<audio src="<?php echo esc_url( $tracks[0]['audioUrl'] ); ?>" controls preload="none" class="cue-audio" style="width: 100%; height: auto"></audio>
@@ -21,4 +23,6 @@
 			</li>
 		<?php endforeach; ?>
 	</ol>
+
+	<?php do_action( 'cue_playlist_bottom', $post, $tracks, $args ); ?>
 </div>
