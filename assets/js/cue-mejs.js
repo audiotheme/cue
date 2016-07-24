2aa4e386aa0d9d8df4e26f444722532e786cdd75
Brady Vercher <brady@blazersix.com>
2016-07-23 22:29:54 -0700

    Insert the logo as an inline SVG instead as a background so the color can be styled.

--- assets/js/cue-mejs.js
+++ assets/js/cue-mejs.js
@@ -3,9 +3,11 @@
 (function( window, $, undefined )  {
 	'use strict';
 
+	var LOGO_ICON = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 40"><path d="M10,40C4.5,40,0,35.5,0,30s4.5-10,10-10s10,4.5,10,10S15.5,40,10,40z M0,0l40,40V20L20,0H0z M80,0H40v14h40V0z M74,40L60,18L46,40H74z"/></svg>';
+
 	$.extend( MediaElementPlayer.prototype, {
 		buildaudiothememark: function( player, controls, layers, media ) {
-			layers.append( '<a href="https://audiotheme.com/?utm_source=cue&utm_medium=link&utm_content=cue-logo&utm_campaign=plugins" target="_blank" class="mejs-audiotheme-mark">AudioTheme</a>' );
+			layers.append( '<a href="https://audiotheme.com/?utm_source=wordpress-plugin&utm_medium=link&utm_content=cue-logo&utm_campaign=plugins" target="_blank" class="mejs-audiotheme-mark">' + LOGO_ICON + '</a>' );
 		},
 
 		buildcuebackground: function( player, controls, layers, media ) {
