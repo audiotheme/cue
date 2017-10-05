c2ccbab3692984b7fb1feda3565db2e0b8d66dbe
Brady Vercher <brady@blazersix.com>
2017-10-05 12:12:58 -0700

    More MediaElement.js 4.x compatibility fixes.

--- assets/js/wp-mediaelement.js
+++ assets/js/wp-mediaelement.js
@@ -6,26 +6,30 @@
 	settings.features.push( 'cuewpmediaelement' );
 
 	MediaElementPlayer.prototype.buildcuewpmediaelement = function( player, controls, layers ) {
-		if ( ! player.container.hasClass( 'wp-audio-shortcode' ) ) {
+		var $container = $( player.container ),
+			$controls = $( controls ),
+			$layers = $( layers );
+
+		if ( ! $container.hasClass( 'wp-audio-shortcode' ) ) {
 			return;
 		}
 
-		var data = JSON.parse( player.container.next( 'script' ).html() );
-		player.container.addClass( 'cue-skin-' + data.theme );
+		var data = JSON.parse( $container.next( 'script' ).html() );
+		$container.addClass( 'cue-skin-' + data.theme );
 
-		layers.append( '<div class="mejs-track-details"><span class="mejs-track-artist"></span><span class="mejs-track-title"></span></div>' );
-		layers.find( '.mejs-track-artist' ).text( data.artist );
-		layers.find( '.mejs-track-title' ).text( data.title );
+		$layers.append( '<div class="mejs-track-details"><span class="mejs-track-artist"></span><span class="mejs-track-title"></span></div>' );
+		$layers.find( '.mejs-track-artist' ).text( data.artist );
+		$layers.find( '.mejs-track-title' ).text( data.title );
 
-		player.$media.on( 'play pause', function() {
-			player.container.toggleClass( 'is-playing', ! player.media.paused );
+		$( player.media ).on( 'play pause', function() {
+			$container.toggleClass( 'is-playing', ! player.media.paused );
 		});
 
-		player.container.on( 'controlsresize', function() {
-			controls.find( '.mejs-time-rail, .mejs-time-slider' ).css( 'width', '' );
+		$container.on( 'controlsresize', function() {
+			$controls.find( '.mejs-time-rail, .mejs-time-slider' ).css( 'width', '' );
 		});
 
-		player.container.addClass(function() {
+		$container.addClass(function() {
 			return ( 'ontouchstart' in window ) || window.DocumentTouch && document instanceof window.DocumentTouch ? 'touch' : 'no-touch';
 		});
 	};
